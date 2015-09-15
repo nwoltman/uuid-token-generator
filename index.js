c6bae8796bdd81f34bb33dd7ba0f97a8148194af
Nathan Woltman <nwoltman@outlook.com>
2015-09-14 22:16:52 -0700

    Polyfill Math.log2() when it is not available

--- index.js
+++ index.js
@@ -9,6 +9,11 @@
 
 var uuid = require('node-uuid');
 
+// Polyfill Math.log2() if necessary
+Math.log2 = Math.log2 || function(x) {
+  return Math.log(x) / Math.log(2);
+};
+
 function KeyGenerator(bitSize, baseEncoding) {
   if (typeof bitSize === 'string') {
     baseEncoding = bitSize;
